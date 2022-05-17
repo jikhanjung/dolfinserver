e26fd8aff441896d0cbd902291673f8602c728b0
Jikhan Jung <honestjung@gmail.com>
2022-05-17 23:05:40 +0900

    first commit

---
+++ dolfinrest/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class DolfinImage(models.Model):
+    ipaddress = models.CharField(max_length=100, blank=True, default='')  #request.META.get('HTTP_X_REAL_IP')
+    user = models.CharField(max_length=100, blank=True, default='')
+    filepath = models.CharField(max_length=200, blank=True, default='')
+    filename = models.CharField(max_length=100, blank=True, default='')
+    md5hash = models.CharField(max_length=200, blank=True, default='')
+    imagefile = models.ImageField(upload_to ='%Y/%m/%d/')
